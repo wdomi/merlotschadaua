f32e85492073b73144322fa92c9330c805e88118
wdomi <20965917+wdomi@users.noreply.github.com>
2025-12-02 21:52:22 +0100

    add foot note for json

--- api/submit.js
+++ api/submit.js
@@ -117,3 +117,10 @@ export default async function handler(req, res) {
   res.setHeader("Allow", ["GET", "POST"]);
   return res.status(405).end("Method Not Allowed");
 }
+
+
+export const config = {
+  api: {
+    bodyParser: true
+  }
+};
